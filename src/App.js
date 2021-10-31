8cdede05a6e2802aa0666eb4407d1b04884e77be
eneskangal <eneskangal@gmail.com>
2021-10-31 19:16:25 +0300

    ayar
    ayar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import "./app.css"
 class App extends Component {
     render() {     
         let variable=()=>{
-            fetch("http://localhost:3004/data")
+            fetch("https://restcountries.com/v3.1/all")
             .then(response => response.json())
-            .then(data => console.log(fonksiyon(data)))  
+            .then(data => fonksiyon(data))  
         }
         let i=0;
-        function fonksiyon(data){            
-            for(i=0; i<data.length; i++){
+        function fonksiyon(data){
+            console.log(data.length)            
+            for(i=0; i<data.length; i++){                         
                 let col=document.createElement("div")
                 col.classList.add("content")
                 document.querySelector(".row").appendChild(col)
@@ -21,23 +22,23 @@ class App extends Component {
                 let img=document.createElement("img")
                 img.classList.add("img-fluid")
                 img.classList.add("card-img-top")
-                img.src=[i+1]+".png";
+                img.src=data[i].flags.png
                 document.querySelectorAll(".card")[i].appendChild(img)
                 let cardBody=document.createElement("div")
                 cardBody.classList.add("card-body")
                 document.querySelectorAll(".card")[i].appendChild(cardBody)
                 let title=document.createElement("h5")
                 title.classList.add("card-title")
-                title.innerHTML=data[i].country
+                title.innerHTML=data[i].name.common
                 document.querySelectorAll(".card-body")[i].appendChild(title)
                 let para=document.createElement("p")
                 para.classList.add("card-text")
-                para.innerHTML=data[i].ınformation
-                document.querySelectorAll(".card-body")[i].appendChild(para)
+                para.innerHTML=data[i].capital
+                document.querySelectorAll(".card-body")[i].appendChild(para)                                             
             }        
         }
         function fonksiyon2(){             
-            let cardTitles=document.querySelectorAll(".card-title")
+            let cardTitles=document.querySelectorAll(".card-title")            
             for(i=0; i<document.querySelectorAll(".card").length; i++){
                 let search=cardTitles[i].innerHTML.search(document.getElementById("ayar").value)                
                 if(search>-1){
